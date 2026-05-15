f4a78b400ab8730a76c43de44c47aca6e545bb5f
TimBLukas <tim.b.lukas@gmail.com>
2026-05-15 22:34:24 +0200

    added structure for jsonx and yamlx

---
+++ filetypes/jsonx/core_functions.py
@@ -0,0 +1,41 @@
+def read_json():
+        pass
+
+
+def write_json():
+        pass
+
+def append_json():
+        pass
+
+def delete_json():
+        pass
+
+def validate_json():
+        pass
+
+def minify_json():
+        pass
+
+def prettify_json():
+        pass
+
+
+def json_to_csv():
+        pass
+
+def csv_to_json():
+        pass
+
+def json_to_xml():
+        pass
+
+def xml_to_json():
+        pass
+
+def json_to_yaml():
+        pass
+
+def yaml_to_json():
+        pass
+
---
+++ filetypes/yamlx/core_functions.py
@@ -0,0 +1,34 @@
+def read_yaml():
+        pass
+
+def write_yaml():
+        pass
+
+def merge_yaml(base, override):
+        pass
+
+def yaml_template():
+        pass
+
+def yaml_to_dict():
+        pass
+
+
+
+def to_json():
+        pass
+
+def to_yaml():
+        pass
+
+def to_xml():
+        pass
+
+def from_json():
+        pass
+
+def from_yaml():
+        pass
+
+def from_xml():
+        pass
